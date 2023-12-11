512586a9297c9f51d2c656b40e204b544d7fd7bb
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-12-11 17:52:15 +0300

    Individual project page redesigned

---
+++ portfolio/pages/scripts/image-changer.js
@@ -0,0 +1,11 @@
+const mainImage = document.querySelector('.main-image');
+const sideImages = document.querySelectorAll('.actual-side-image');
+
+mainImage.src = sideImages[0].src;
+
+sideImages.forEach(sideImage => {
+    sideImage.addEventListener('click', () => {
+        const src = mainImage.src;
+        mainImage.src = sideImage.src;
+    });
+});
