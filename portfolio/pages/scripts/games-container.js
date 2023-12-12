9470fd77b6e8ef6b84cc9f3674da96ce8afe0af7
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-12-12 13:43:19 +0300

    .

--- portfolio/pages/scripts/games-container.js
+++ portfolio/pages/scripts/games-container.js
@@ -2,3 +2,16 @@ function ClickIt(value){
     localStorage.setItem('gameIndex', value);
     console.log(localStorage.getItem('gameIndex'));
 }
+const games = [
+    "No Name Survivor",
+    "Tetris"
+];
+
+const gamesContainer = document.querySelector('.games-container');
+let endHTML = "";
+games.forEach((game, index) => {
+    endHTML += `<a onclick="ClickIt(${index})" href="../pages/game-pages/project-page.html">${game}</a>
+    <br><br>`;
+
+});
+gamesContainer.innerHTML = endHTML;
