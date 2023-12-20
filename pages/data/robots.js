d0b3fb9e506903d7e0bfe413dbe72a72d177daa4
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-12-20 15:15:55 +0300

    New images for uipath projects

--- pages/data/robots.js
+++ pages/data/robots.js
@@ -1,7 +1,7 @@
 const robots = [
     {
         id: 0,
-        projectName: "Clothing Consultant",
+        projectName: "Tweet Scraper",
         actualPlatforms: [
             {
                 platform: "-",
@@ -9,18 +9,18 @@ const robots = [
             }
         ],
         projectImages: [
-            "../../images/uipath-projects/image_1.jpg",
-            "../../images/uipath-projects/image_2.jpg",
-            "../../images/uipath-projects/image_3.jpg",
-            "../../images/uipath-projects/image_4.jpg",
-            "../../images/uipath-projects/image_5.jpg"
+            "../../images/projects-pics/robot-pics/robot-15/1.1.jpg",
+            "../../images/projects-pics/robot-pics/robot-15/1.1.jpg",
+            "../../images/projects-pics/robot-pics/robot-15/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-15/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-15/3.jpg"
         ],
         actualExplanation: "Explanation",
         projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
     },
     {
         id: 1,
-        projectName: "Data Capturing",
+        projectName: "Random CV Creator",
         actualPlatforms: [
             {
                 platform: "-",
@@ -28,18 +28,18 @@ const robots = [
             }
         ],
         projectImages: [
-            "../../images/uipath-projects/image_2.jpg",
-            "../../images/uipath-projects/image_1.jpg",
-            "../../images/uipath-projects/image_3.jpg",
-            "../../images/uipath-projects/image_4.jpg",
-            "../../images/uipath-projects/image_5.jpg"
+            "../../images/projects-pics/robot-pics/robot-14/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-14/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-14/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-14/3.jpg",
+            "../../images/projects-pics/robot-pics/robot-14/4.jpg",
         ],
         actualExplanation: "Explanation",
         projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
     },
     {
         id: 2,
-        projectName: "Web Searching",
+        projectName: "Calendar Picker",
         actualPlatforms: [
             {
                 platform: "-",
@@ -47,18 +47,18 @@ const robots = [
             }
         ],
         projectImages: [
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/2.jpg",
-            "../../images/projects-pics/game-pics/tetris/4.jpg",
-            "../../images/projects-pics/game-pics/tetris/3.jpg"
+            "../../images/projects-pics/robot-pics/robot-13/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-13/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-13/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-13/3.jpg",
+            "../../images/projects-pics/robot-pics/robot-13/4.jpg",
         ],
         actualExplanation: "Explanation",
         projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
     },
     {
         id: 3,
-        projectName: "Web Searching",
+        projectName: "Lead Generation",
         actualPlatforms: [
             {
                 platform: "-",
@@ -66,18 +66,18 @@ const robots = [
             }
         ],
         projectImages: [
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/2.jpg",
-            "../../images/projects-pics/game-pics/tetris/4.jpg",
-            "../../images/projects-pics/game-pics/tetris/3.jpg"
+            "../../images/projects-pics/robot-pics/robot-12/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-12/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-12/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-12/3.jpg",
+            "../../images/projects-pics/robot-pics/robot-12/4.jpg",
         ],
         actualExplanation: "Explanation",
         projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
     },
     {
         id: 4,
-        projectName: "Web Searching",
+        projectName: "Image BG Remover",
         actualPlatforms: [
             {
                 platform: "-",
@@ -85,13 +85,32 @@ const robots = [
             }
         ],
         projectImages: [
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/1.jpg",
-            "../../images/projects-pics/game-pics/tetris/2.jpg",
-            "../../images/projects-pics/game-pics/tetris/4.jpg",
-            "../../images/projects-pics/game-pics/tetris/3.jpg"
+            "../../images/projects-pics/robot-pics/robot-11/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-11/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-11/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-11/3.jpg",
+            "../../images/projects-pics/robot-pics/robot-11/4.jpg",
         ],
         actualExplanation: "Explanation",
         projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
-    }
+    },
+    {
+        id: 5,
+        projectName: "IP Location Converter",
+        actualPlatforms: [
+            {
+                platform: "-",
+                link: "#"
+            }
+        ],
+        projectImages: [
+            "../../images/projects-pics/robot-pics/robot-9/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-9/1.jpg",
+            "../../images/projects-pics/robot-pics/robot-9/2.jpg",
+            "../../images/projects-pics/robot-pics/robot-9/3.jpg",
+            "../../images/projects-pics/robot-pics/robot-9/4.jpg",
+        ],
+        actualExplanation: "Explanation",
+        projectVideo: "https://www.youtube.com/embed/yQ9Ydz7jo6I"
+    },
 ];
