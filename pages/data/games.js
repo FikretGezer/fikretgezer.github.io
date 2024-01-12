0e962b310b0eeb8619322f24bcaa78d04f187032
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2024-01-12 04:23:52 +0300

    Tetris Explanation Update

--- pages/data/games.js
+++ pages/data/games.js
@@ -212,19 +212,17 @@ const games = [
         ],
         actualExplanation: `
         <h3>What is this game about?</h3>
-        <br>
         <ul>
         <li> • Tetris is a classic arcade game. It's actually one of 4 the horsemen(Tetris, Breakout, Mario, Pacman) as I called. The reason I called them like this is, all of these games teach different aspect of the game development.</li>
         <li> • Players' goal is creating perfectly and horizontally aligned lines without any gaps with random shaped falling blocks. As blocks fall, player can rotate or move them inside of the board and each time players create a perfect horizontal line, that line dissapears and players earn points.</li>
         <li> • Primary objective is to keep the board clean as much possible and preventthe blocks reaching the top of the screen.</li>
         </ul>
-        <br><br>
+        <br>
 
         <h3>Background and Development</h3>
-        <br>
         &nbsp;&nbsp;&nbsp; Creating the Tetris taught me the some fundamental things in the game development. For example, I had never created a scoreboard until I made Tetris. And also I had to figure out;
-        <br><br>
         <ul>
+        <br>
         <li>&nbsp;&nbsp;&nbsp; • How to create dynamic sized boards</li>
         <li>&nbsp;&nbsp;&nbsp; • How to keep blocks inside of the board while moving and rotating them</li>
         <li>&nbsp;&nbsp;&nbsp; • How to identify perfect lines before destroying them</li>
